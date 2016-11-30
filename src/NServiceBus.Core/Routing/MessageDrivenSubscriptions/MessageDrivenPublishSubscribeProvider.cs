429e1d5e1e6b2f9b7c9f6fe4235fa350ef874bfb
Tim Bussmann <tim.bussmann@particular.net>
2016-11-30 12:38:29 +0100

    rebase on develop

--- src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/MessageDrivenPublishSubscribeProvider.cs
+++ src/NServiceBus.Core/Routing/MessageDrivenSubscriptions/MessageDrivenPublishSubscribeProvider.cs
@@ -3,6 +3,8 @@ namespace NServiceBus.Features
     using System;
     using ObjectBuilder;
     using Persistence;
+    using Routing;
+    using Routing.MessageDrivenSubscriptions;
     using Transport;
     using Unicast.Messages;
     using Unicast.Subscriptions.MessageDrivenSubscriptions;
@@ -11,14 +13,16 @@ namespace NServiceBus.Features
     {
         public Func<IBuilder, IPublishRouter> GetRouter(FeatureConfigurationContext context)
         {
-            var distributionPolicy = context.Routing.DistributionPolicy;
+            var distributionPolicy = context.Settings.Get<DistributionPolicy>();
+            var publishers = context.Settings.Get<Publishers>();
+            var enforceBestPractices = context.Settings.Get<bool>(RoutingFeature.EnforceBestPracticesSettingsKey);
 
             ConfiguredPublishers configuredPublishers;
             if (context.Settings.TryGet(out configuredPublishers))
             {
                 var conventions = context.Settings.Get<Conventions>();
 
-                configuredPublishers.Apply(context.Routing.Publishers, conventions, context.Routing.EnforceBestPractices);
+                configuredPublishers.Apply(publishers, conventions, enforceBestPractices);
             }
 
             return builder => new UnicastPublishRouter(
@@ -34,8 +38,11 @@ namespace NServiceBus.Features
                 throw new Exception("The selected persistence doesn't have support for subscription storage. Select another persistence or disable the message-driven subscriptions feature using endpointConfiguration.DisableFeature<MessageDrivenSubscriptions>()");
             }
 
+            var publishers = context.Settings.Get<Publishers>();
+            var endpointInstances = context.Settings.Get<EndpointInstances>();
+
             var transportInfrastructure = context.Settings.Get<TransportInfrastructure>();
-            var subscriptionRouter = new SubscriptionRouter(context.Routing.Publishers, context.Routing.EndpointInstances, i => transportInfrastructure.ToTransportAddress(LogicalAddress.CreateRemoteAddress(i)));
+            var subscriptionRouter = new SubscriptionRouter(publishers, endpointInstances, i => transportInfrastructure.ToTransportAddress(LogicalAddress.CreateRemoteAddress(i)));
             var distributorAddress = context.Settings.GetOrDefault<string>("LegacyDistributor.Address");
             var subscriberAddress = distributorAddress ?? context.Settings.LocalAddress();
 
