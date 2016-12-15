15695d22a38e804b29477f7afcc4a79502fc972b
Tim Bussmann <tim.bussmann@particular.net>
2016-12-15 18:25:29 +0100

    tests

---
+++ src/NServiceBus.AcceptanceTests/Sagas/When_polymorphic_saga_updates_saga.cs
@@ -0,0 +1,121 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+    using NUnit.Framework;
+
+    public class When_polymorphic_saga_updates_saga : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_store_all_changes()
+        {
+            var context = await Scenario.Define<TestContext>()
+                .WithEndpoint<MyEndpoint>(e => e
+                    .When(s => s.SendLocal(new StartSagaMessage() {MessageId = Guid.NewGuid()})))
+                .Done(ctx => ctx.HandledTimeoutMessage >= 1)
+                .Run();
+
+            Assert.AreEqual(1, context.HandledStartMessage);
+            Assert.AreEqual(1, context.HandledTimeoutMessage);
+
+            Assert.IsTrue(context.StoredBaseHandlerValue);
+            Assert.IsTrue(context.StoredChildHandlerValue);
+        }
+
+        class TestContext : ScenarioContext
+        {
+            public int HandledStartMessage;
+            public int HandledTimeoutMessage;
+
+            public bool StoredBaseHandlerValue { get; set; }
+            public bool StoredChildHandlerValue { get; set; }
+        }
+
+        class MyEndpoint : EndpointConfigurationBuilder
+        {
+            public MyEndpoint()
+            {
+                EndpointSetup<DefaultServer>(s => s.EnableFeature<TimeoutManager>());
+
+            }
+
+            class SagaData : ContainSagaData
+            {
+                public Guid SagaId { get; set; }
+                public bool BaseHandlerValue { get; set; }
+                public bool ChildHandlerValue { get; set; }
+            }
+
+            class MySaga : Saga<SagaData>,
+                IAmStartedByMessages<StartSagaMessage>,
+                IHandleMessages<BaseUpdateSagaMessage>,
+                IHandleMessages<ChildUpdateSagaMessage>,
+                IHandleTimeouts<TimeoutMessage>
+            {
+                public TestContext TestContext { get; set; }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+                {
+                    mapper.ConfigureMapping<StartSagaMessage>(m => m.MessageId).ToSaga(s => s.SagaId);
+                    mapper.ConfigureMapping<BaseUpdateSagaMessage>(m => m.MessageId).ToSaga(s => s.SagaId);
+                    mapper.ConfigureMapping<ChildUpdateSagaMessage>(m => m.MessageId).ToSaga(s => s.SagaId);
+                }
+
+                public async Task Handle(StartSagaMessage message, IMessageHandlerContext context)
+                {
+                    Interlocked.Increment(ref TestContext.HandledStartMessage);
+
+                    await context.SendLocal(new ChildUpdateSagaMessage
+                    {
+                        MessageId = message.MessageId
+                    });
+
+                    await RequestTimeout<TimeoutMessage>(context, DateTime.Now.AddSeconds(15));
+                }
+
+                public Task Handle(BaseUpdateSagaMessage message, IMessageHandlerContext context)
+                {
+                    Data.BaseHandlerValue = true;
+                    return Task.FromResult(0);
+                }
+
+                public Task Handle(ChildUpdateSagaMessage message, IMessageHandlerContext context)
+                {
+                    Data.ChildHandlerValue = true;
+                    return Task.FromResult(0);
+                }
+
+                public Task Timeout(TimeoutMessage message, IMessageHandlerContext context)
+                {
+                    Interlocked.Increment(ref TestContext.HandledTimeoutMessage);
+                    TestContext.StoredBaseHandlerValue = Data.BaseHandlerValue;
+                    TestContext.StoredChildHandlerValue = Data.ChildHandlerValue;
+                    return Task.FromResult(0);
+                }
+            }
+
+        }
+
+        class StartSagaMessage : ICommand
+        {
+            public Guid MessageId { get; set; }
+        }
+
+        class BaseUpdateSagaMessage : ICommand
+        {
+            public Guid MessageId { get; set; }
+        }
+
+        class ChildUpdateSagaMessage : BaseUpdateSagaMessage
+        {
+        }
+
+        class TimeoutMessage : ICommand
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.AcceptanceTests/Sagas/When_starting_saga_from_inherited_message.cs
@@ -0,0 +1,105 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_polymorphic_saga_starts_saga : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_store_state_of_both_handlers()
+        {
+            var ctx = await Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(e => e
+                    .When(s => s.SendLocal(new StartChildMessage
+                    {
+                        SomeId = Guid.NewGuid(),
+                    })))
+                .Done(c => c.ReceivedFollowUpMessages >= 2)
+                .Run();
+
+            Assert.AreEqual(2, ctx.ReceivedFollowUpMessages);
+            Assert.IsTrue(ctx.InvokedBaseHandler);
+            Assert.IsTrue(ctx.InvokedChildHandler);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public int ReceivedFollowUpMessages;
+            public bool InvokedBaseHandler { get; set; }
+            public bool InvokedChildHandler { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(s => s.Recoverability().Immediate(x => x.NumberOfRetries(0)));
+            }
+
+            public class SagaData : ContainSagaData
+            {
+                public Guid SomeId { get; set; }
+            }
+
+            class MySaga : Saga<SagaData>,
+                IAmStartedByMessages<StartBaseMessage>,
+                IAmStartedByMessages<StartChildMessage>,
+                IHandleMessages<FollowUpMessage>
+            {
+                public Context TestContext { get; set; }
+
+                public async Task Handle(StartBaseMessage message, IMessageHandlerContext context)
+                {
+                    TestContext.InvokedBaseHandler = true;
+                    await context.SendLocal(new FollowUpMessage()
+                    {
+                        SomeId = Data.SomeId
+                    });
+                }
+
+                public async Task Handle(StartChildMessage message, IMessageHandlerContext context)
+                {
+                    TestContext.InvokedChildHandler = true;
+                    await context.SendLocal(new FollowUpMessage()
+                    {
+                        SomeId = Data.SomeId
+                    });
+                }
+
+                public Task Handle(FollowUpMessage message, IMessageHandlerContext context)
+                {
+                    Interlocked.Increment(ref TestContext.ReceivedFollowUpMessages);
+                    return Task.FromResult(0);
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+                {
+                    mapper.ConfigureMapping<StartBaseMessage>(m => m.SomeId).ToSaga(s => s.SomeId);
+                    mapper.ConfigureMapping<StartChildMessage>(m => m.SomeId).ToSaga(s => s.SomeId);
+                    mapper.ConfigureMapping<FollowUpMessage>(m => m.SomeId).ToSaga(s => s.SomeId);
+                }
+            }
+        }
+
+        public class StartBaseMessage : ICommand
+        {
+            public Guid SomeId { get; set; }
+            public Guid SomeOtherId { get; set; }
+        }
+
+        public class StartChildMessage : StartBaseMessage
+        {
+        }
+
+        public class FollowUpMessage : ICommand
+        {
+            public Guid SomeId { get; set; }
+        }
+    }
+
+
+}
